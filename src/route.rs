056f2567a29b92980ca25bd30922bb9933ce5bbe
xmh0511 <970252187@qq.com>
2023-08-29 11:18:05 +0800

    update

--- src/route.rs
+++ src/route.rs
@@ -1,19 +1,25 @@
-
 mod hello;
 
 use salvo::Router;
 use serde_json::json;
 
-
 use crate::web_core::{authorization::AuthGuard, error_catch::HttpErrorKind};
+
+use salvo::cors::{Cors, CorsHandler};
+use salvo::http::Method;
 
 #[allow(unused_macros)]
 macro_rules! create_router {
-	($($m:ident)::*,$method:ident) => {
-		Router::with_path(acquire_last_ident!($($m)*)).$method($($m)::*)
+	($($m:ident)::*,$($method:ident),+) => {
+		//Router::with_path(acquire_last_ident!($($m)*)).$method($($m)::*)
+		create_router!(IN Router::with_path(acquire_last_ident!($($m)*)), $($m)::* , $($method),+)
 	};
-	($prefix:literal,$($m:ident)::*,$method:ident)=>{
-		Router::with_path(format!("{}{}",$prefix,acquire_last_ident!($($m)*))).$method($($m)::*)
+	($prefix:literal,$($m:ident)::*,$($method:ident),+)=>{
+		//Router::with_path(format!("{}{}",$prefix,acquire_last_ident!($($m)*))) $(. $method( $($m)::*  ))+
+		create_router!(IN Router::with_path(format!("{}{}",$prefix,acquire_last_ident!($($m)*))), $($m)::* , $($method),+)
+	};
+	(IN $e:expr, $m:path , $($method:ident),+)=>{
+		$e $(.$method($m))+
 	};
 }
 #[allow(unused_macros)]
@@ -26,19 +32,28 @@ macro_rules! acquire_last_ident {
 	}
 }
 
-
-
-pub fn gen_router(_secret_key:String)->Router{
-	let api_router = Router::new();
-	let r = create_router!("a/b/",hello::ab::show,get);
-	let hello = create_router!(hello::hello,get).hoop(AuthGuard::new(|_a|{
-		HttpErrorKind::Json(json!({
-			"status":"fail",
-			"msg":"unauthorized"
-		}))
-	}));
-	let login = Router::with_path("login").get(hello::login);
-	api_router.push(hello).push(login).push(r)
+pub fn gen_router(_secret_key: String) -> Router {
+    let api_router = Router::new();
+    let r = create_router!("a/b/", hello::ab::show, get, post).hoop(build_cros("*"));
+    let hello = create_router!(hello::hello, get, post).hoop(AuthGuard::new(|_a| {
+        HttpErrorKind::Json(json!({
+            "status":"fail",
+            "msg":"unauthorized"
+        }))
+    }));
+    let login = Router::with_path("login").get(hello::login);
+    api_router.push(hello).push(login).push(r)
 }
-
-
+#[allow(dead_code)]
+pub fn build_cros(allow_origin: &str) -> CorsHandler {
+    Cors::new()
+        .allow_origin(allow_origin)
+        .allow_methods(vec![
+            Method::GET,
+            Method::POST,
+            Method::DELETE,
+            Method::PUT,
+            Method::PATCH,
+        ])
+        .into_handler()
+}
