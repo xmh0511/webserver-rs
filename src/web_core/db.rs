c875727eeee94adfa45971d4fe0a291ca4350b91
xmh0511 <970252187@qq.com>
2024-04-22 17:51:53 +0800

    update document

--- src/web_core/db.rs
+++ src/web_core/db.rs
@@ -18,11 +18,11 @@ async fn init_db_pool(db_info: Vec<DataBaseInfo>) -> Result<(), DbErr> {
     Ok(())
 }
 #[allow(dead_code)]
-#[cfg(any(feature = "mysql", feature = "sqlite", feature = "postgres"))]
 #[cfg_attr(
     docsrs,
     doc(cfg(any(feature = "mysql", feature = "sqlite", feature = "postgres")))
 )]
+#[cfg(any(feature = "mysql", feature = "sqlite", feature = "postgres"))]
 /// Get database connection by indexing `name` of `[[database]]` set in `config.toml`
 pub fn get(key: &str) -> Result<&'static DatabaseConnection, std::io::Error> {
     DBCONN_POOL
